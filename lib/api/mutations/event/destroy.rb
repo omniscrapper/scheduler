6089c18472e5bcb957cf21f9da95af36de0dcdfb
Artem Rakov <scoff1k@gmail.com>
2019-11-11 21:01:11 -0500

    destroy action added

---
+++ lib/api/mutations/event/destroy.rb
@@ -0,0 +1,23 @@
+require_relative '../base'
+require_relative '../../../models/event'
+
+module Mutations
+  module Event
+    class Destroy < Mutations::Base
+      argument :id, ID, required: true
+
+      field :success, Boolean, null: false
+      field :errors, [String], null: true
+
+      def resolve(id:)
+        event = ::Event.find(id)
+
+        if event.destroy
+          { success: true }
+        else
+          { success: false, errors: event.errors.full_messages }
+        end
+      end
+    end
+  end
+end
--- lib/api/types/mutation.rb
+++ lib/api/types/mutation.rb
@@ -1,10 +1,12 @@
 require_relative 'base'
 require_relative '../mutations/event/create'
 require_relative '../mutations/event/update'
+require_relative '../mutations/event/destroy'
 
 module Types
   class Mutation < Types::Base
     field :createEvent, mutation: Mutations::Event::Create
     field :updateEvent, mutation: Mutations::Event::Update
+    field :destroyEvent, mutation: Mutations::Event::Destroy
   end
 end
