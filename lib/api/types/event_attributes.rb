80e43bf43bd2bad7dacdd6946e2dfb82ef0dddba
Artem Rakov <scoff1k@gmail.com>
2019-11-11 21:13:07 -0500

    change permissions on event attributes

--- lib/api/types/event_attributes.rb
+++ lib/api/types/event_attributes.rb
@@ -2,7 +2,7 @@ require_relative 'base_input'
 
 module Types
   class EventAttributes < Types::BaseInput
-    argument :name, String, required: true
-    argument :task_id, Integer, required: true
+    argument :name, String, required: false
+    argument :task_id, Integer, required: false
   end
 end
